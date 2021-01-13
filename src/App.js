b7537ab48c643e2d0b9cae1928102812116bf424
payalgupta20 <1706428@kiit.ac.in>
2021-01-13 05:36:08 +0530

    External Styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import Header from "./Header";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <div className="header">Phone Directory</div>
+        <Header/>
         <button> Add </button>
 
         <div>
@@ -18,3 +19,4 @@ class App extends Component {
 }
 
 export default App;
+
